a4ed15609d37e193f09e32e2d52b4a2edf5aebaf
Alexander Sulfrian <alexander@sulfrian.net>
2014-03-04 16:22:51 +0000

    Travis build 15 pushed to gh-pages

---
+++ build15-a19/source/buildConfig/debug/org/thoughtcrime/securesms/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.thoughtcrime.securesms;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "org.thoughtcrime.securesms";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 66;
+}
